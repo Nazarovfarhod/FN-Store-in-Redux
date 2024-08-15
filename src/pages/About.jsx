d434bdf52e5b1ad8999df2e6a8922b12e9537910
farxodnazarov <farhodnazarov002gmail.com>
2024-08-15 17:34:02 +0500

    changed about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,8 +1,12 @@
-
 function About() {
   return (
-    <div>About</div>
-  )
+    <div className="ml-60">
+      <h3 className="mt-40 text-4xl text-center font-medium w-[900px]">
+        Our store has products for men and women. Our products are discounted
+        from 5% to 25%. We are glad that you chose our store.
+      </h3>
+    </div>
+  );
 }
 
-export default About
+export default About;
