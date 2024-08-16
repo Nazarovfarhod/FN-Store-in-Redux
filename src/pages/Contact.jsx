a5e6fea656a6e1ee99b2d96da70be34d58e0386d
farxodnazarov <farhodnazarov002gmail.com>
2024-08-16 09:53:07 +0500

    added text to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,11 @@
-
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <h1 className="text-5xl font-bold w-[900px] mx-auto mt-40">
+        This page is under construction. It will start working soon
+      </h1>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
